ee66ae41e5882cfce8fb946bb94befc64db637d4
anhtuan <anhtuan20011997@gmail.com>
2018-07-05 11:15:02 +0700

    + add print hi

--- src/Main.java
+++ src/Main.java
@@ -86,6 +86,7 @@ public class Main {
         System.out.println("Hello");
         System.out.println("Hello");
         System.out.println("Hello");
+        System.out.println("Hi");
     }
 
     public static void main4(String[] args) {
